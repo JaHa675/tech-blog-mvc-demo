0afe8cd6f5516d7f20e0b012fdc3c84697f8b84b
James Harding <99635678+JaHa675@users.noreply.github.com>
2022-05-09 19:19:04 -0700

    first commit

---
+++ controllers/frontEndRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const {User,Blog} = require('../models');
+
+router.get("/",(req,res)=>{
+    Blog.findAll().then(blogs=>{
+        console.log(blogs)
+        const hbsBlogs = blogs.map(blog=>blog.get({plain:true}))
+        console.log("==========")
+        console.log(hbsBlogs)
+        res.render("home",{blogs:hbsBlogs})
+    })
+})
+
+router.get("/login",(req,res)=>{
+    res.render("login")
+})
+
+router.get("/profile",(req,res)=>{
+    if(!req.session.user){
+        return res.redirect("/login")
+    }
+    User.findByPk(req.session.user.id,{
+        include:[Blog]
+    }).then(userData=>{
+        console.log(userData);
+        const hbsData = userData.get({plain:true})
+        console.log("=======")
+        console.log(hbsData);
+        res.render("profile",hbsData)
+    })
+})
+
+module.exports = router;
